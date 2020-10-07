483bc08593b97b0e6cbe48edc223bba61fa8257f
Pablo Ferrari <pablo.ferrari@me.com>
2020-10-06 23:54:51 -0300

    UPD // Fixes

--- app/Classes/InstitutionService.php
+++ app/Classes/InstitutionService.php
@@ -19,17 +19,18 @@ class InstitutionService
 
     public static function createInstitution($data)
     {
-        $new = new Institution();
-        $new->name = $data['name']; 
-        $new->email = $data['email']; 
-        $new->phone = $data['phone']; 
-        $new->cuit = $data['cuit']; 
-        $new->image = $data['image']; 
-        $new->active = $data['active']; 
-        $new->plan_id = $data['plan_id']; 
-        $new->city_id = $data['city_id']; 
-        $new->save();
-        return Institution::where('id', $new->id)->with(['plan', 'city.province.country'])->first();
+        $newInstitution = new Institution();
+        $newInstitution->name = $data['name']; 
+        $newInstitution->email = $data['email']; 
+        $newInstitution->phone = $data['phone']; 
+        $newInstitution->cuit = $data['cuit'] ?? null; 
+        $newInstitution->image = $data['image'] ?? null; 
+        $newInstitution->active = $data['active'] ?? null; 
+        $newInstitution->plan_id = $data['plan_id'] ?? null; 
+        $newInstitution->city_id = $data['city_id'] ?? null; 
+        $newInstitution->active = true; 
+        $newInstitution->save();
+        return $newInstitution;
     }
 
     public static function updateInstitution($id, $data)
