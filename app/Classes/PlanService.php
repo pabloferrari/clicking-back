8e042b188ff96ce7ba6f38d6c93417691d546282
Daniel Gudiño <daniel.jabba1@gmail.com>
2020-10-08 15:44:24 -0400

    Plans Fix

--- app/Classes/PlanService.php
+++ app/Classes/PlanService.php
@@ -22,19 +22,24 @@ class PlanService
     {
         $newPlan = new Plan();
         $newPlan->name = $data['name'];
+        $newPlan->active = 1; // true
         $newPlan->save();
         return $newPlan;
     }
 
     public static function updatePlan($id, $data)
     {
-        Plan::where('id', $id)->update($data);
-        return Plan::where('id', $id)->first();
+        // Plan::where('id', $id)->update($data);
+        // return Plan::where('id', $id)->first();
+        $plan = Plan::where('id', $id)->first();
+        $plan->name   = $data['name'];
+        $plan->active = $data['active'];
+        $plan->save();
+        return $plan;
     }
-    
+
     public static function deletePlan($id)
     {
         return Plan::where('id', $id)->delete();
     }
-    
 }
