7309538fc35cc9b6452b073a56c7d2e71b71ca47
Nestor Infante <nestorlinfante@gmail.com>
2020-10-12 12:56:40 -0400

    Add files resolve conflicts

---
+++ app/Http/Middleware/StudentValidate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class StudentValidate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!Auth::user()->hasRole('student')){
+            return response()->json(["message" => "Unauthorized"], 403);
+        }
+        return $next($request);
+    }
+}
