6ec74c8ca658decbdf5839d733bfeed0762e3e08
Daniel Gudiño <dgudino@mary-iancarina.com>
2020-11-06 17:02:15 -0400

    Add Classroom - Course Detail

--- app/Classes/ClassroomService.php
+++ app/Classes/ClassroomService.php
@@ -3,6 +3,9 @@
 namespace App\Classes;
 
 use App\Models\Classroom;
+use App\Classes\CourseService;
+use Log;
+use DB;
 
 class ClassroomService
 {
@@ -19,22 +22,71 @@ class ClassroomService
 
     public static function createClassroom($data)
     {
-        $new = new Classroom();
-        $new->name     = $data['name'];
-        $new->shift_id = $data['shift_id'];
-        $new->institution_id = $data['institution_id'];
-        $new->save();
-        return self::getClassroom($new->id);
+        DB::beginTransaction();
+        try{
+            // Insert Classroom
+            $new = new Classroom();
+            $new->name     = $data['name'];
+            $new->shift_id = $data['shift_id'];
+            $new->institution_id = $data['institution_id'];
+            $new->save();
+
+            // Insert Course
+            foreach ($data['courses'] as $key) {
+                $key['classroom_id'] = $new->id;
+                $courseService = new CourseService();
+                $courseService->createCourse($key);
+                Log::debug(__METHOD__ . ' -> NEW COURSE ' . json_encode($key));
+            }
+
+            // Insert Student
+            $new->classroomStudentsPivot()->attach($data['student_id']);
+            Log::debug(__METHOD__ . ' -> NEW CLASSROOM STUDENT ' . json_encode($new));
+
+            DB::commit();
+            Log::debug(__METHOD__ . ' -> NEW CLASSROOM ' . json_encode($new));
+            return self::getClassroom($new->id);
+        } catch (\Exception $e) {
+            DB::rollback();
+            Log::error(__METHOD__ . ' - ROLLBACK CLASSROOM -> ' . json_encode($data) . ' exception: ' . $e->getMessage());
+            return false;
+        }
     }
 
     public static function updateClassroom($id, $data)
     {
-        $Classroom = Classroom::find($id);
-        $Classroom->name     = $data['name'];
-        $Classroom->shift_id = $data['shift_id'];
-        $Classroom->institution_id = $data['institution_id'];
-        $Classroom->save();
-        return self::getClassroom($Classroom->id);
+        DB::beginTransaction();
+        try{
+            $Classroom = Classroom::find($id);
+            $Classroom->name     = $data['name'];
+            $Classroom->shift_id = $data['shift_id'];
+            $Classroom->institution_id = $data['institution_id'];
+
+            if ( $Classroom->save() ){
+
+                // $newData = [];
+                // foreach ($data['courses'] as $key) {
+                //     $newData = [
+                //         'classroom_student_id' => $key,
+                //     ];
+                //     $newAssignment->studentAssignments()->attach($newAssignment->id, $newData);
+                // }
+
+
+                DB::commit();
+                Log::debug(__METHOD__ . ' -> NEW CLASSROOM ' . json_encode($Classroom));
+                return self::getClassroom($Classroom->id);
+            }else{
+                DB::rollback();
+                Log::error(__METHOD__ . ' - ROLLBACK user -> ' . json_encode($data) . ' exception: ' . $e->getMessage());
+                return false;
+            }
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            Log::error(__METHOD__ . ' - ROLLBACK user -> ' . json_encode($data) . ' exception: ' . $e->getMessage());
+            return false;
+        }
     }
 
     public static function deleteClassroom($id)
