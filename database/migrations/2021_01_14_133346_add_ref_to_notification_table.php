cf1f5988b62172cbac719e9523755d6cdefaa96a
Pablo Ferrari <pablo.ferrari@me.com>
2021-01-14 10:35:17 -0300

    UPD // Fixes

---
+++ database/migrations/2021_01_14_133346_add_ref_to_notification_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRefToNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->integer('model_id')->default(0)->after('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            //
+        });
+    }
+}
